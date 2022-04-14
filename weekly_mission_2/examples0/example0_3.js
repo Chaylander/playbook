6973e96120126cc85300c93299c80d5d453fc122
Chaylander <robotrjj@hotmail.com>
2022-04-13 22:40:59 -0500

    Agregando el ejemplo3 de examples_0.js

---
+++ weekly_mission_2/examples0/example0_3.js
@@ -0,0 +1,20 @@
+// Ejemplo 3: Objeto con diferentes propiedades
+const myObject3 = {
+    name: "Ketsi",
+    age: 2,
+    nicknames: [
+        "Randys",
+        "Randalicia"
+    ],
+    address: {
+      zip_code: "44300",
+      street: "Av. batallon de san patricio",
+      city: "Guadalajara"
+    }
+}
+console.log("Ejemplo 3: Objeto con diferentes propiedades")
+console.log(myObject3)
+console.log(myObject3.name)
+console.log(myObject3["address"])
+console.log(myObject3.address)
+ 
