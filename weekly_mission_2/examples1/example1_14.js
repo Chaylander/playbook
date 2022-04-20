0b3fbee0da553049b256718b57aec0dfd0c24d66
RobertoJáuregui <robotrjj@hotmail.com>
2022-04-20 12:33:47 -0500

    Agrego ejemplo 15 de examples_1.js y comentario en ejemplo 14

--- weekly_mission_2/examples1/example1_14.js
+++ weekly_mission_2/examples1/example1_14.js
@@ -5,5 +5,5 @@
 const bools = [true, true, true, true]
 // Uso de Some para ver si al menos uno de los elementos es false
 const areSomeTrue = bools.some((b) =>  b === false)
-//si uno d ellos es false, enviara true
+//si uno de ellos es false, enviara true
 console.log("Ejemplo 14: Alguno de los elementos en el array es false: " + areSomeTrue) //true
