66f263e945744612161ff40bdcce07cb135b38ec
RobertoJáuregui <robotrjj@hotmail.com>
2022-04-19 22:39:12 -0500

    Agregando archivo index.js, clase ajolonauta y script start en .json

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,10 @@
+const Ajolonauta = require('./app/ajolonauta');
+
+//creando el objeto e instanciando la clase
+const ketsi = new Ajolonauta("Ketsi");
+const woopa = new Ajolonauta("Woopa");
+const konnichiwa = new Ajolonauta("Konnichiwa");
+
+console.log(ketsi.name);
+console.log(woopa.name);
+console.log(konnichiwa.name);
