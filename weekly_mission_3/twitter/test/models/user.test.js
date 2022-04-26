380eb561d653d2a3619445bc223eaf8b0d74b454
RobertoJáuregui <robotrjj@hotmail.com>
2022-04-26 13:59:15 -0500

    Agrego pruebas faltantes clase user

--- weekly_mission_3/twitter/test/models/user.test.js
+++ weekly_mission_3/twitter/test/models/user.test.js
@@ -4,5 +4,10 @@ describe("Test para user", () => {
     test("Creación de user", () => {
         const user = new User(1, "chaylander", "Roberto Jauregui", "Bio", "dateCreated", "lastUpdated");
         expect(user.id).toBe(1);
+        expect(user.username).toBe("chaylander");
+        expect(user.name).toBe("Roberto Jauregui");
+        expect(user.bio).toBe("Bio");
+        expect(user.dateCreated).toBe("dateCreated");
+        expect(user.lastUpdated).toBe("lastUpdated");
     })
 })
