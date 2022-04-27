618d4f60e5e3a2c0433de5b859a4d301361324b9
RobertoJáuregui <robotrjj@hotmail.com>
2022-04-26 22:31:19 -0500

    Agrego requerimientos 2 al 4 y sus respectivas pruebas

--- weekly_mission_3/twitter/app/views/UserView.js
+++ weekly_mission_3/twitter/app/views/UserView.js
@@ -1,3 +1,4 @@
+const UserService = require('./../../app/services/UserService');
 const user = require("./../models/User");
 
 class UserView{
@@ -5,6 +6,18 @@ class UserView{
         if (payload === null){
             return {error: "payload no existe"}
         }
+        else if (payload.username === null || payload.name === null || payload.id === null){
+            return {error: "necesitan tener un valor valido"}
+        }
+        else if (payload.username === undefined || payload.name === undefined || payload.id === undefined){
+            return {error: "necesitan tener un valor valido"}
+        }
+        else{
+            return UserService.create(payload.id, payload.username, payload.name); //si enviamos payload no funciona
+        }
+        
+
+
     }
     
 }   
--- weekly_mission_3/twitter/test/views/UserView.test.js
+++ weekly_mission_3/twitter/test/views/UserView.test.js
@@ -1,10 +1,33 @@
 const UserView = require('./../../app/views/UserView');
 
-describe("Test for userView", () => {
+
+describe("Requirement 1: Test for userView", () => {
     test("Return an object when try to create a new user with a null payload", () => {
         const payload = null;
         const result  = UserView.createUser(payload);
         expect(result.error).toMatch(/payload no existe/)
     })
+
+    test("Requirement 2: Return an error object when try to create a new user with a payload with invalid properties", () => {
+        const payload = {username: null, name: 12, id: "id"}
+        const result = UserView.createUser(payload);
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
+
+    })
+
+    test("Requirement 3: Return an error object when try to create a new user with a payload with missing propierties", () => {
+        const payload = {username: "username"}
+        const result = UserView.createUser(payload);
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
+    })
+
+    test("Requirement 4: Create a user by a given valid payload", () => {
+        const payload = {username: "username", id: 1, name: "name"}
+        const result = UserView.createUser(payload);
+        expect(result.name).toBe("name");
+        expect(result.username).toBe("username");
+        expect(result.id).toBe(1);
+    })
+
 
 })
