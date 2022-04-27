3c8d9268c86cdcc52b75a92d0b4a7b0ecb5ac6e2
RobertoJáuregui <robotrjj@hotmail.com>
2022-04-26 20:44:02 -0500

    Agrego requerimiento 2 del sprint 2) services

--- weekly_mission_3/twitter/app/services/userService.js
+++ weekly_mission_3/twitter/app/services/userService.js
@@ -4,6 +4,9 @@ class UserService {
     static create(id, username, name){
         return new user(id, username, name, "Sin bio");
     }
+    static getInfo(user){//retorna un array con los datos del user
+        return [user.id, user.username, user.name, "sin bio"];
+    }
 }
 
 module.exports = UserService;
--- weekly_mission_3/twitter/test/services/userService.test.js
+++ weekly_mission_3/twitter/test/services/userService.test.js
@@ -10,4 +10,14 @@ describe("Test para userservice", () => {
         expect(user.bio).not.toBeUndefined();
     })
 
+    test("Requerimiento 2: Get all user data in a list" ,() => {
+        
+        const user = UserService.create(1, "Chaylander", "Roberto Jauregui")
+        const userInfoInList = UserService.getInfo(user)
+        expect(userInfoInList[0]).toBe(1);
+        expect(userInfoInList[1]).toBe("Chaylander");
+        expect(userInfoInList[2]).toBe("Roberto Jauregui");
+        expect(userInfoInList[3]).not.toBeUndefined();
+    })
+
 })
