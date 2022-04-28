ff89bdb33b7b7ec3bcdba5fac6c8d12dadfcd8db
RobertoJáuregui <robotrjj@hotmail.com>
2022-04-28 11:44:54 -0500

    Agregando nuevo route /launchx

--- weekly_mission_3/express_basic/app.js
+++ weekly_mission_3/express_basic/app.js
@@ -5,10 +5,21 @@ const app = express();
 //Puerto en el que se vera la app en el navegador, localhost:3000
 const port = 3000;
 
+//Ctrl + C para parar el servidor
+
 //path inicial, respondera a la url localhost:3000
 app.get('/', (req, res) => {
     res.send("Hello World!");
 })
+
+//Agregar una nueva ruta
+//localhost:3000/launchx
+app.get('/launchx', (req, res) => {
+    res.send("Bienvenidos a LaunchX!");
+})
+
+
+
 
 //con esto iniciamos el servidor
 app.listen(port, () => {
